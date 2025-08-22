af87470fb559681fb2cc6c7aa052798ab220177a
Antony <antony@gmail.com>
2025-08-22 18:13:07 -0300

    commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+
+// variaveis
+
+let menu = document.getElementById('menu');
+let select = document.getElementById('numCards');
+let button = document.getElementById('start');
+
+// loop inicia 4. menor ou igual a 10. incrementa 2
+
+// variavel - condição - ação
+for(let i = 4;i<=10;i +=2 ){
+    // valor do select multiplicado por ele mesmo
+    let n = i * i;
+
+// opções do usuario no select
+
+let op = document.createElement('option');
+
+//setar o valor para o op
+op.value = n;       //propriedade value
+op.innerHTML = n;   //mostrar na tela o valor
+
+// colocar op como filho do select
+select.appendChild(op);
+
+}
+
+
