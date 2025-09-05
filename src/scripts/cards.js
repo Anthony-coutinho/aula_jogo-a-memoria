5d6d595eb102ef41cff65fb245950364bd27a2ba
Antony <antony@gmail.com>
2025-09-05 18:54:36 -0300

    elaboração de todos os metodos dos gerenciador das cartas

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -12,8 +12,37 @@ class CardManager {
         let template = document.getElementById("cardTemplate"); //referenciar o template
         let clone = template.content.cloneNode(true); //criar clone
 
+        clone.children[0].addEventListener("click",
+            event => this.onClick(event)
+        )
+
         //retornar o clone
         return clone
 
     }
+    //Captura o eventos de clique
+    onClick(event){
+    this.flip(event.target);
+    }
+    //vira a carta
+    flip(cardNode){
+
+    cardNode.children[0].classList.add("selected");
+
+    }
+    //desvira a carta
+    unFlip(cardNode){
+
+    cardNode.children[0].classList.remove("selected");
+
+    }
+    //método que não deixa o usuario virar duas cartas
+    disable(cardNode){
+    cardNode.children[0].classList.add("matched");
+    this.unFlip(cardNode)
+    
+        
+    }
+
 }
+
